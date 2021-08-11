51b6df766b5cf46db843de0120146ba036b3dc87
jihyunlee <jihyunlee.me@gmail.com>
2021-08-11 11:24:53 +0900

    functional 컴포넌트로 만드는 카운터

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-function App() {
+import React, { useState } from 'react';
+
+const App = () => {
+  const [item, setItem] = useState(0);
+  const increase = () => setItem(item + 1);
+  const decrease = () => setItem(item - 1);
+
   return (
-    <div className="App"> hello world </div>
+    <div className="App">
+      <h2 className="App"> hello world {item}</h2>
+      <button onClick={increase}>increse</button>
+      <button onClick={decrease}>decrese</button>
+    </div>
   );
-}
+};
 
 export default App;
