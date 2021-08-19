56df713e30d7ea3df6067a27808afd8f15464a7d
dream_JHL <jihyunlee.me@gmail.com>
2021-08-19 17:29:40 +0900

    #1.1 #1.2 useInput 사용하기 / validator 사용하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { useState } from 'react';
 
-const useIput = () => { };
+/* eslint-disable no-shadow */
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = event => {
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === 'function') {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
 
 const App = () => {
+  const maxLen = value => value.length <= 50 && !value.includes('@');
+  const name = useInput('Ms.', maxLen);
   return (
     <div className="App">
       <h2>Hello</h2>
+      {/* <input placeholder="Name" value={name.value} onChange={name.onChange} /> */}
+      <input placeholder="Name" {...name} />
     </div>
   );
 };
