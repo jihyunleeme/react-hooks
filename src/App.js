fe216b0ecdedee051942ad03f84157bbfce177b8
dream_JHL <jihyunlee.me@gmail.com>
2021-08-19 14:58:49 +0900

    #1.1 useInput 초기세팅

--- src/App.js
+++ src/App.js
@@ -1,49 +1,13 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [item, setItem] = useState(0);
-  const increase = () => setItem(item + 1);
-  const decrease = () => setItem(item - 1);
+const useIput = () => { };
 
+const App = () => {
   return (
     <div className="App">
-      <h2 className="App"> hello world {item}</h2>
-      <button onClick={increase}>increse</button>
-      <button onClick={decrease}>decrese</button>
+      <h2>Hello</h2>
     </div>
   );
 };
 
-class AppUgly extends React.Component {
-  state = {
-    item: 1,
-  };
-
-  render() {
-    const { item } = this.state;
-    return (
-      <div className="App">
-        <h2 className="App"> hello world {item}</h2>
-        <button onClick={this.increase}>increse</button>
-        <button onClick={this.decrease}>decrese</button>
-      </div>
-    );
-  }
-
-  increase = () => {
-    this.setState(state => {
-      return {
-        item: state.item + 1,
-      };
-    });
-  };
-
-  decrease = () => {
-    this.setState(state => {
-      return {
-        item: state.item - 1,
-      };
-    });
-  };
-}
-export default AppUgly;
+export default App;
