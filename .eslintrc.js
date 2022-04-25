d23848557623baa9282f89ab45c14dd6856e17c4
jihyunlee <jihyunlee.me@gmail.com>
2022-04-26 01:42:13 +0900

    [setting] eslint, prettier 설정 변경에 따른 수정사항

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,32 @@
 module.exports = {
-  env: {
-    browser: true,
-  },
-  extends: ['airbnb-base', 'plugin:prettier/recommended'],
-  parserOptions: {
-    ecmaVersion: 7,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  parser: 'babel-eslint',
-  plugins: ['react', 'prettier', 'import'],
-  rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'auto',
+   env: {
+      browser: true,
+      commonjs: true,
+      es6: true,
+   },
+   extends: ['airbnb-base', 'plugin:prettier/recommended'],
+   parserOptions: {
+      ecmaVersion: 7,
+      sourceType: 'module',
+      ecmaFeatures: {
+         jsx: true,
       },
-    ],
-    'no-extra-semi': 'error',
-    'no-unused-vars': 'off',
-    'import/prefer-default-export': 'off',
-    'no-console': 'off',
-    'no-alert': 'off',
-    'no-underscore-dangle': 'off',
-    'import/no-named-as-default': 0,
-  },
+   },
+   parser: 'babel-eslint',
+   plugins: ['react', 'prettier', 'import'],
+   rules: {
+      'prettier/prettier': [
+         'error',
+         {
+            endOfLine: 'auto',
+         },
+      ],
+      'no-extra-semi': 'error',
+      'no-unused-vars': 'off',
+      'import/prefer-default-export': 'off',
+      'no-console': 'off',
+      'no-alert': 'off',
+      'no-underscore-dangle': 'off',
+      'import/no-named-as-default': 0,
+   },
 };
